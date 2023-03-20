f6120e0640f32a3af4dfab83255b139eca6f3788
jssatorres <juansataz.dev@gmail.com>
2023-03-20 18:38:53 +0100

    feat (main) introduction point

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+const { setMainMenu } = require('./menu.js')
+const path = require('path')
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minWidth: 400,
+    minHeight: 300,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  mainWindow.loadFile('index.html')
+  setMainMenu(mainWindow)
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+// app.on('window-all-closed', () => {
+//   if (process.platform !== 'darwin') app.quit()
+// })
+
+// darwin->macos
+// linux->linux
+// win32->window
